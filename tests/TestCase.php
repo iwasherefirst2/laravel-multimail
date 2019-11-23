9a14de8e2101697220403b5ee911f0fd8fca5b45
Adam Nielsen <1765602+iwasherefirst2@users.noreply.github.com>
2019-11-23 18:23:17 +0100

    Refactor test class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,26 @@
 
 namespace IWasHereFirst2\LaravelMultiMail\Tests;
 
+use IWasHereFirst2\LaravelMultiMail\Facades\MultiMail;
+
 use IWasHereFirst2\LaravelMultiMail\MultiMailServiceProvider;
+use Swift_Events_EventListener;
+use Swift_Message;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected $emails;
+
     public function setUp(): void
     {
         parent::setUp();
+
+        MultiMail::registerPlugin(new TestingMailEventListener($this));
+    }
+
+    public function addEmail(Swift_Message $email)
+    {
+        $this->emails[] = $email;
     }
 
     /**
@@ -38,5 +51,20 @@ class TestCase extends \Orchestra\Testbench\TestCase
             'database' => ':memory:',
             'prefix'   => '',
         ]);
+    }
+}
+
+class TestingMailEventListener implements Swift_Events_EventListener
+{
+    protected $test;
+
+    public function __construct($test)
+    {
+        $this->test = $test;
+    }
+
+    public function beforeSendPerformed($event)
+    {
+        $this->test->addEmail($event->getMessage());
     }
 }
