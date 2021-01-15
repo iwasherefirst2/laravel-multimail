956e7f79f5ea64c62ce75d7921a8de0569e68a59
Lourival-ETI-BR <lourival@lourival.eti.br>
2021-01-14 22:40:25 -0300

    Update MultiMailServiceProvider.php
    Use same key 'mail_settings_class' to verify is custom class is provided.

--- src/MultiMailServiceProvider.php
+++ src/MultiMailServiceProvider.php
@@ -23,7 +23,7 @@ class MultiMailServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind('iwasherefirst2-laravelmultimail', function () {
-            if(config()->has('multimail.config_class')){
+            if(config()->has('multimail.mail_settings_class')){
                 $configClass = config('multimail.mail_settings_class');
                 $config = new $configClass();
             }
