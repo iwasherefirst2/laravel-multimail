98f7cf8dc8d23a36daa9b99e868501ab08342227
Adam Nielsen <info@drnielsen.de>
2023-10-12 22:27:37 +0200

    Cleanup configuration

--- publishable/config/multimail.php
+++ publishable/config/multimail.php
@@ -9,26 +9,16 @@ return [
     | Enjoy a life with multimail
     |
     */
-    'use_default_mail_facade_in_tests' => true,
 
     'emails'  => [
         'office@example.com' => [
-            'pass'          => env('MAIL_PASSWORD'),
-            'username'      => env('MAIL_USERNAME'),
             'from_name'     => 'Max Musterman',
             'reply_to_mail' => 'reply@example.com',
         ],
         'contact@example.net'  => [
-            'pass'     => env('second_mail_password'),
+            'driver'        => 'smtp',
+            'from_name'     => 'The Hive',
+            'reply_to_mail' => 'contact@example.com',
         ],
     ],
-
-    'provider' => [
-        'default' => [
-            'host'      => env('MAIL_HOST'),
-            'port'      => env('MAIL_PORT'),
-            'encryption' => env('MAIL_ENCRYPTION'),
-        ],
-    ],
-
 ];
